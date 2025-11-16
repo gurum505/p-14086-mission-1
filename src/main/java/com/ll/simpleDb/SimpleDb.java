03c860fb9f8049a6ac18f316b9a096597f5b4e0b
gurum505 <sayhojeong505@gmail.com>
2025-11-16 23:15:59 +0900

    TDD t001 red ::  Sql클래스, SimpleDb method init

--- src/main/java/com/ll/simpleDb/SimpleDb.java
+++ src/main/java/com/ll/simpleDb/SimpleDb.java
@@ -1,4 +1,27 @@
 package com.ll.simpleDb;
 
 public class SimpleDb {
+    private String localhost;
+    private String username;
+    private String password;
+    private String dbName;
+
+    public SimpleDb(String localhost, String username, String password, String dbName) {
+        this.localhost = localhost;
+        this.username = username;
+        this.password = password;
+        this.dbName = dbName;
+    }
+
+    public void setDevMode(boolean b) {
+    }
+
+    public void run(String s){}
+
+    public void run(String s, String title, String body, boolean isBlind) {
+    }
+
+    public Sql genSql() {
+        return new Sql("");
+    }
 }
--- src/main/java/com/ll/simpleDb/Sql.java
+++ src/main/java/com/ll/simpleDb/Sql.java
@@ -1,4 +1,25 @@
 package com.ll.simpleDb;
 
 public class Sql {
+    private String _sql;
+
+    public Sql (String s) {
+        _sql = s;
+    }
+
+    public long insert() {
+        return 0;
+    }
+
+    public Sql append(String s){
+        _sql += s;
+        return new Sql(_sql);
+    }
+
+    public Sql append(String s1,String s2){
+        _sql += s1;
+        _sql += s2;
+        return new Sql(_sql);
+    }
+
 }
