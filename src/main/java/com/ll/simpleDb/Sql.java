906925d5c57666b6b58258e7b6f4c635d320b197
gurum505 <sayhojeong505@gmail.com>
2025-11-17 10:26:05 +0900

    TDD t003 red :: SQL param 추가

--- src/main/java/com/ll/simpleDb/Sql.java
+++ src/main/java/com/ll/simpleDb/Sql.java
@@ -1,10 +1,27 @@
 package com.ll.simpleDb;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Sql {
     private String _sql;
+    public List<Object> _values;
 
-    public Sql (String s) {
-        _sql = s;
+    public Sql (String sql) {
+        _sql = sql;
+        _values= new ArrayList<>();
+        _values.add(null);
+    }
+    public Sql (String sql, List<Object> values) {
+        this._sql = sql;
+        this._values= values;
+    }
+
+    public Sql append(String s1,Object... args){
+        _sql += s1;
+        _values.addAll(Arrays.asList(args));
+        return new Sql(_sql);
     }
 
     public long insert() {
@@ -12,15 +29,11 @@ public class Sql {
         return 1;
     }
 
-    public Sql append(String s){
-        _sql += s;
-        return new Sql(_sql);
+    public int update() {
+        return 1;
     }
 
-    public Sql append(String s1,String[] args){
-        _sql += s1;
-        //_sql += s2;
-        return new Sql(_sql);
+    public int delete() {
+        return 1;
     }
-
 }
