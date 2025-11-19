86ec59d6729b00b77340d56111964150d9cbb84e
gurum505 <sayhojeong505@gmail.com>
2025-11-19 14:32:25 +0900

    refactor: mapper위치를 sql로 옮김

--- src/main/java/com/ll/simpleDb/SimpleDb.java
+++ src/main/java/com/ll/simpleDb/SimpleDb.java
@@ -1,7 +1,6 @@
 package com.ll.simpleDb;
 
 import java.sql.*;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -28,26 +27,17 @@ public class SimpleDb {
 
     }
 
-    private Connection getCurrentThreadConnection() {
-        Connection conn = connections.get(Thread.currentThread().getName());
-        if (conn != null) {
-            return conn;
-        }
-        try {
-            conn = DriverManager.getConnection(this.url, this.username, this.password);
-            connections.put(Thread.currentThread().getName(), conn);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return conn;
+    public Sql genSql() {
+        return new Sql(this);
     }
 
     public void setDevMode(boolean b) {
     }
 
-    public Sql genSql() {
-        return new Sql(this);
-    }
+    // ===================================
+    // DDL Public API 영역
+    // 초기 데이터 생성 및 테스트
+    // ===================================
 
     public int run(Sql _sql) {
         return run(_sql.get_sql(), _sql.get_values().toArray());
@@ -70,19 +60,12 @@ public class SimpleDb {
         return rs;
     }
 
-    private void setParam(PreparedStatement pstmt, int index, Object param) throws SQLException {
-        if (param instanceof String) {
-            pstmt.setString(index, (String) param);
-        } else if (param instanceof Integer) {
-            pstmt.setInt(index, (int) param);
-        } else if (param instanceof Long) {
-            pstmt.setLong(index, (long) param);
-        } else if (param instanceof Boolean) {
-            pstmt.setBoolean(index, (boolean) param);
-        }
-    }
+    // ===================================
+    // DML Public API 영역
+    // 데이터 조회
+    // ===================================
 
-    public <T> T temp(Sql _sql, Function<ResultSet, T> mapper) {
+    public <T> T selectCommon(Sql _sql, Function<ResultSet, T> mapper) {
         T object = null;
         Connection conn = getCurrentThreadConnection();
         try (PreparedStatement pstmt = conn.prepareStatement(_sql.get_sql())) {
@@ -91,6 +74,7 @@ public class SimpleDb {
             for (int i = 0; i < _sqlParams.size(); i++) {
                 setParam(pstmt, i + 1, _sqlParams.get(i));
             }
+
             ResultSet rs = pstmt.executeQuery();
             object = mapper.apply(rs);
 
@@ -102,67 +86,23 @@ public class SimpleDb {
         return object;
     }
 
+    // ===================================
+    // Connection 영역
+    // connection 연결 및 리소스 해제 수동조작
+    // ===================================
 
-    public List<Map<String, Object>> selectRows(Sql _sql) {
-        return temp(_sql, resultSet -> {
-            try {
-                return selectRowsMapper(resultSet);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
-
-    private List<Map<String, Object>> selectRowsMapper(ResultSet rs) throws SQLException {
-        List<Map<String, Object>> data = new ArrayList<>();
-        ResultSetMetaData rsmd = rs.getMetaData();
-        while (rs.next()) {
-            Map<String, Object> row = new HashMap<>();
-            for (int i = 1; i <= rsmd.getColumnCount(); i++) {
-                row.put(rsmd.getColumnName(i), rs.getObject(i));
-            }
-            data.add(row);
+    private Connection getCurrentThreadConnection() {
+        Connection conn = connections.get(Thread.currentThread().getName());
+        if (conn != null) {
+            return conn;
         }
-        return data;
-    }
-
-    public Object selectSingle(Sql _sql) {
-        return temp(_sql, (resultSet -> {
-            try {
-                return selectSingleMapper(resultSet);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }));
-    }
-
-    private Object selectSingleMapper(ResultSet rs) throws SQLException {
-        Object object = null;
-        if (rs.next()) {
-            object = rs.getObject(1);
+        try {
+            conn = DriverManager.getConnection(this.url, this.username, this.password);
+            connections.put(Thread.currentThread().getName(), conn);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-        return object;
-    }
-
-    public List<Long> selectLongs(Sql _sql) {
-        return temp(_sql, resultSet -> {
-            try {
-                return selectLongsMapper(resultSet);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
-
-    private List<Long> selectLongsMapper(ResultSet rs) throws SQLException {
-        List<Long> cnt = new ArrayList<>();
-        ResultSetMetaData rsmd = rs.getMetaData();
-        while (rs.next()) {
-            for (int i = 1; i <= rsmd.getColumnCount(); i++) {
-                cnt.add(rs.getLong(i));
-            }
-        }
-        return cnt;
+        return conn;
     }
 
     public void close() {
@@ -208,6 +148,23 @@ public class SimpleDb {
             throw new RuntimeException(e);
         } finally {
             close();
+        }
+    }
+
+    // ===================================
+    // pstmt 세팅
+    // ?에 param의 요소 순차적으로 삽입
+    // ===================================
+
+    private void setParam(PreparedStatement pstmt, int index, Object param) throws SQLException {
+        if (param instanceof String) {
+            pstmt.setString(index, (String) param);
+        } else if (param instanceof Integer) {
+            pstmt.setInt(index, (int) param);
+        } else if (param instanceof Long) {
+            pstmt.setLong(index, (long) param);
+        } else if (param instanceof Boolean) {
+            pstmt.setBoolean(index, (boolean) param);
         }
     }
 }
--- src/main/java/com/ll/simpleDb/Sql.java
+++ src/main/java/com/ll/simpleDb/Sql.java
@@ -4,6 +4,9 @@ import com.ll.Article;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -39,6 +42,15 @@ public class Sql {
         return this;
     }
 
+    public String get_sql() {
+        return _sql.toString();
+    }
+
+    // ===================================
+    // DDL API 영역
+    // simpleDb.run 사용
+    // ===================================
+
     public long insert() {
         return (long) simpleDb.run(this);
     }
@@ -51,24 +63,56 @@ public class Sql {
         return simpleDb.run(this);
     }
 
+    // ===================================
+    // DML Public API 영역
+    // simpleDb.selectCommon 사용
+    // mapper를 삽입해 적절한 리턴값으로 변환한다.
+    // ===================================
+
+    public List<Map<String, Object>> selectRows() {
+        return simpleDb.selectCommon(this, resultSet -> {
+            try {
+                return selectRowsMapper(resultSet);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    public <T> List<T> selectRows(Class<T> cls) {
+        if (cls == Article.class) {
+            return simpleDb.selectCommon(this, resultSet -> {
+                        try {
+                            return selectRowsMapper(resultSet);
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .stream().map(row -> cls.cast(new Article(row))).toList();
+        }
+        return null;
+    }
+
     public Map<String, Object> selectRow() {
-        return simpleDb.selectRows(this).getFirst();
+        return selectRows().getFirst();
     }
 
     public <T> T selectRow(Class<T> cls) {
         return selectRows(cls).getFirst();
     }
 
-    public List<Map<String, Object>> selectRows() {
-        return simpleDb.selectRows(this);
+    public List<Long> selectLongs() {
+        return simpleDb.selectCommon(this, resultSet -> {
+            try {
+                return selectLongsMapper(resultSet);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
-    public <T> List<T> selectRows(Class<T> cls) {
-        if (cls == Article.class) {
-            return simpleDb.selectRows(this)
-                    .stream().map(row -> cls.cast(new Article(row))).toList();
-        }
-        return null;
+    public Long selectLong() {
+        return selectSingle(Long.class);
     }
 
     public LocalDateTime selectDatetime() {
@@ -83,16 +127,20 @@ public class Sql {
         return selectSingle(Boolean.class);
     }
 
-    public Long selectLong() {
-        return selectSingle(Long.class);
-    }
-
-    public List<Long> selectLongs() {
-        return simpleDb.selectLongs(this);
-    }
-
+    // ===================================
+    // private 영역
+    // simpleDb.selectCommon 에 사용되는 Mapper
+    // <T> T selectSingle
+    // ===================================
     private <T> T selectSingle(Class<T> cls) {
-        Object object = simpleDb.selectSingle(this);
+        Object object = simpleDb.selectCommon(this, (resultSet -> {
+            try {
+                return selectSingleMapper(resultSet);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }));
+
         if (cls == Boolean.class && object instanceof Number) {
             return ((Number) object).intValue() == 1 ? (T) Boolean.TRUE : (T) Boolean.FALSE;
         }
@@ -102,7 +150,37 @@ public class Sql {
         return null;
     }
 
-    public String get_sql() {
-        return _sql.toString();
+    private List<Map<String, Object>> selectRowsMapper(ResultSet rs) throws SQLException {
+        List<Map<String, Object>> data = new ArrayList<>();
+        ResultSetMetaData rsmd = rs.getMetaData();
+        while (rs.next()) {
+            Map<String, Object> row = new HashMap<>();
+            for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                row.put(rsmd.getColumnName(i), rs.getObject(i));
+            }
+            data.add(row);
+        }
+        return data;
     }
+
+    private List<Long> selectLongsMapper(ResultSet rs) throws SQLException {
+        List<Long> cnt = new ArrayList<>();
+        ResultSetMetaData rsmd = rs.getMetaData();
+        while (rs.next()) {
+            for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                cnt.add(rs.getLong(i));
+            }
+        }
+        return cnt;
+    }
+
+    private Object selectSingleMapper(ResultSet rs) throws SQLException {
+        Object object = null;
+        if (rs.next()) {
+            object = rs.getObject(1);
+        }
+        return object;
+    }
+
+
 }
